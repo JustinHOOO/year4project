d65e571546cc84d86c911fbe1f50f9551a075eec
JustinHOOO <2031237393@qq.com>
2024-05-26 06:09:36 +0800

    service v2 (impl）

--- question_sys/questionnaireBackEnd/src/main/java/com/example/questionnaire/service/impl/FillInServiceImpl.java
+++ question_sys/questionnaireBackEnd/src/main/java/com/example/questionnaire/service/impl/FillInServiceImpl.java
@@ -11,6 +11,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,12 +19,14 @@ import java.util.Date;
 
 @Service
 public class FillInServiceImpl implements FillInService {
-    private final AnswerDao answerDao;
-    private final QuestionnaireIpDao questionnaireIpDao;
-    private final QuestionnaireDao questionnaireDao;
-    private final Gson gson;
+    final
+    AnswerDao answerDao;
+    final
+    QuestionnaireIpDao questionnaireIpDao;
+    final
+    QuestionnaireDao questionnaireDao;
+    final Gson gson;
 
-    @Autowired
     public FillInServiceImpl(AnswerDao answerDao, QuestionnaireIpDao questionnaireIpDao, Gson gson, QuestionnaireDao questionnaireDao) {
         this.answerDao = answerDao;
         this.questionnaireIpDao = questionnaireIpDao;
@@ -34,68 +37,64 @@ public class FillInServiceImpl implements FillInService {
     @Override
     @Transactional
     public void submitAnswer(Integer questionnaireId, String answerListJson, String ip) {
-        // Save the IP addresses of the survey respondents
-        saveQuestionnaireIp(questionnaireId, ip);
 
-        // Check if the survey already closed
-        Questionnaire questionnaire = questionnaireDao.findByQuestionnaireId(questionnaireId);
-        if ("closed".equals(questionnaire.getStatus())) {
-            return;
-        }
-
-        // Record the number of participants (+1)
-        incrementFillCount(questionnaire);
-
-        // Process answers
-        JsonArray answerListArray = gson.fromJson(answerListJson, JsonArray.class);
-        answerListArray.forEach(answerElement -> saveAnswer(answerElement.getAsJsonObject(), ip));
-    }
-
-    private void saveQuestionnaireIp(Integer questionnaireId, String ip) {
+        // save the IP addresses of the survey respondents
         QuestionnaireIp questionnaireIp = new QuestionnaireIp();
         questionnaireIp.setIp(ip);
         questionnaireIp.setQuestionnaireId(questionnaireId);
         questionnaireIpDao.save(questionnaireIp);
-    }
 
-    private void incrementFillCount(Questionnaire questionnaire) {
-        questionnaire.setFillCount((questionnaire.getFillCount() == null ? 0 : questionnaire.getFillCount()) + 1);
+        // check if the survey already closed
+        Questionnaire questionnaire = questionnaireDao.findByQuestionnaireId(questionnaireId);
+        if (questionnaire.getStatus().equals("closed")) return ;
+
+            // Record the number of participants（+1).
+            if (questionnaire.getFillCount() == null) questionnaire.setFillCount(0);
+        questionnaire.setFillCount(questionnaire.getFillCount() + 1);
         questionnaireDao.save(questionnaire);
-    }
 
-    private void saveAnswer(JsonObject oneAnswerObject, String ip) {
-        Answer answer = new Answer();
-        answer.setIp(ip);
-        answer.setFillTime(new Date());
-        answer.setQuestionId(oneAnswerObject.get("questionId").getAsInt());
-        answer.setQuestionTitle(oneAnswerObject.get("questionTitle").getAsString());
-        answer.setQuestionType(oneAnswerObject.get("questionType").getAsString());
-        answer.setWriteValue(getWriteValue(oneAnswerObject, answer.getQuestionType()));
-        answerDao.save(answer);
-    }
+        // process answers
+        JsonArray answerListArray = gson.fromJson(answerListJson, JsonArray.class);
+        for (JsonElement oneAnswer : answerListArray
+        ) {
+            Answer answer = new Answer();
+            answer.setIp(ip);
+            answer.setFillTime(new Date());
 
-    private String getWriteValue(JsonObject oneAnswerObject, String questionType) {
-        switch (questionType) {
-            case "single_check":
-                return oneAnswerObject.get("answerSingleCheck").getAsString();
-            case "multi_check":
-                return oneAnswerObject.get("answerMultiCheck").toString();
-            case "single_line_text":
-            case "multi_line_text":
-                return oneAnswerObject.get("answerText").getAsString();
-            case "number":
-                return oneAnswerObject.get("answerNumber").toString();
-            case "grade":
-                return oneAnswerObject.get("answerGrade").toString();
-            case "date":
-                return oneAnswerObject.get("answerDate").toString();
-            default:
-                throw new IllegalArgumentException("Unknown question type: " + questionType);
+            JsonObject oneAnswerObject = oneAnswer.getAsJsonObject();
+            answer.setQuestionId(oneAnswerObject.get("questionId").getAsInt());
+            answer.setQuestionTitle(oneAnswerObject.get("questionTitle").getAsString());
+            String questionType = oneAnswerObject.get("questionType").getAsString();
+            answer.setQuestionType(questionType);
+
+            if (questionType.equals("single_check")) {
+                answer.setWriteValue(oneAnswerObject.get("answerSingleCheck").getAsString());
+            } else if (questionType.equals("multi_check")) {
+                answer.setWriteValue(oneAnswerObject.get("answerMultiCheck").toString());
+            } else if (questionType.equals("single_line_text")) {
+                answer.setWriteValue(oneAnswerObject.get("answerText").getAsString());
+            } else if (questionType.equals("multi_line_text")) {
+                answer.setWriteValue(oneAnswerObject.get("answerText").getAsString());
+            } else if (questionType.equals("number")) {
+                answer.setWriteValue(oneAnswerObject.get("answerNumber").toString());
+            } else if (questionType.equals("grade")) {
+                answer.setWriteValue(oneAnswerObject.get("answerGrade").toString());
+            } else if (questionType.equals("date")) {
+                answer.setWriteValue(oneAnswerObject.get("answerDate").toString());
+            }
+
+            answerDao.save(answer);
         }
+
+
     }
+
 
     @Override
     public Boolean checkAlreadySubmit(Integer questionnaireId, String ip) {
-        return !questionnaireIpDao.findAllByQuestionnaireIdAndIp(questionnaireId, ip).isEmpty();
+        System.out.println(questionnaireIpDao.findAllByQuestionnaireIdAndIp(questionnaireId, ip));
+        if (questionnaireIpDao.findAllByQuestionnaireIdAndIp(questionnaireId, ip).size() > 0) {//已经提交过
+            return true;
+        } else return false;
     }
 }
